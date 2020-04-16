33685c2b711e6a6b9f1269e3aac1367fb628bd34
Marcel <fleuren.marcel@gmail.com>
2020-04-16 11:20:10 +0200

    added env keys

--- controllers/database.js
+++ controllers/database.js
@@ -1,5 +1,4 @@
 const admin = require('firebase-admin')
-// const serviceAccount = require('../serviceAccount.json')
 admin.initializeApp({
   credential: admin.credential.cert({
     "type": process.env.TYPE,
