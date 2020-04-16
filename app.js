50282ce360f97c55c79488c26839d247e96a7bcd
Marcel <fleuren.marcel@gmail.com>
2020-04-16 11:37:55 +0200

    filled in process env on the port number

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const session = require("express-session")({
   saveUninitialized: true
 })
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 require('dotenv').config()
 
